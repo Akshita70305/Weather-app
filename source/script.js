5c3e882c41304847712a90a32c9bc867ee6f490f
Akshita70305 <154804207+Akshita70305@users.noreply.github.com>
2024-12-22 12:41:27 +0530

    Added Weather API

--- source/script.js
+++ source/script.js
@@ -1,8 +1,24 @@
+function updateWeather(response) {
+  let tempElement = document.querySelector("#temp");
+  let temperature = response.data.temperature.current;
+  tempElement.innerHTML = Math.round(temperature);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+}
+
+function addApi(city) {
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function addCityName(event) {
   event.preventDefault();
   let citySearch = document.querySelector("#city-search");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = citySearch.value;
+
+  addApi(citySearch.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
